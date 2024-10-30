f32fcb1bbaa9d3798e662f88ff5606f6f45c6eb0
semkufu95 <semkufu95@gmail.com>
2024-10-30 18:56:34 +0300

    LRU cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+'''
+A module LRUCache
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    ''' Inherits from Base Model
+    Uses  last recently used to cache data
+    '''
+    def __init__(self):
+        ''' Initialize
+        '''
+        super().__init__()
+        self.cache = []  # A list of cached cache
+
+    def put(self, key, item):
+        '''
+        Assign a lru cache
+        '''
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            #  Remove key from cache if already exists
+            self.cache.remove(key)
+
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            lru_key = self.cache.pop(0)
+            del self.cache_data[lru_key]
+            print(f"DISCARD: {lru_key}")
+
+        self.cache_data[key] = item  # Add item to cache
+        self.cache.append(key)  # update cache
+
+    def get(self, key):
+        ''' retrieves the keys in self.cache_data
+        '''
+        if key is None or key not in self.cache_data:
+            return None
+
+        # move accessed key to the end to mark as recent used
+        self.cache.remove(key)
+        self.cache.append(key)
+
+        return self.cache_data[key]
