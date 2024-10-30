617d4bba3d94cd64c7dd73125226c53f5f979dc8
semkufu95 <semkufu95@gmail.com>
2024-10-30 17:42:51 +0300

    LIFO caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+'''
+A LIFOCache module
+'''
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    '''
+    A class that inherits from BaseCaching class
+    It implements the LIFO algorithm
+    '''
+    def __init__(self):
+        '''
+        Initialize
+        '''
+        super().__init__()
+        self.cache = [] # A list of cached data.
+
+    def put(self, key, item):
+        '''
+        Assign item to cache_data
+        '''
+        if key is None or item is None:
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            last_key = self.cache.pop(-1)
+            del self.cache_data[last_key] # delete the last item
+            print(f'DISCARD: {last_key}')
+
+        self.cache_data[key] = item
+        self.cache.append(key)
+
+    def get(self, key):
+        '''
+        Retrieves value of self.cache_data linked to key
+        '''
+        return self.cache_data(key, None)
